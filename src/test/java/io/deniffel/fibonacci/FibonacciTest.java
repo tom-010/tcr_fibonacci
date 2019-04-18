cd1a0f0302dc9f6ec5283f627b5ab72bc448d60a
Thomas Deniffel <tdeniffel@acm.org>
2019-04-18 22:56:20 +0200

    working

--- src/test/java/io/deniffel/fibonacci/FibonacciTest.java
+++ src/test/java/io/deniffel/fibonacci/FibonacciTest.java
@@ -2,11 +2,12 @@ package io.deniffel.fibonacci;
 
 import org.junit.Test;
 
+import static io.deniffel.fibonacci.Fibonacci.fib;
 import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
     @Test
     public void zero_zero() {
-        assertEquals(0, Fibonacci.fib(0));
+        assertEquals(0, fib(0));
     }
 }
